aee58c46d2ff3134566f1c5a331b9014ca612718
Jorge Garcia <jegcelorio@yahoo.com>
2019-01-01 13:33:28 -0600

    Create Node.cs
    Node Class

---
+++ LinkedList/Node.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Node
+    {
+       public Node next = null;
+       public int data;
+
+      public Node(int d)
+        {
+            data = d; 
+        }
+    }
+}
